ac60d50d168b14bd559e2558f846711734debf0b
Rafay <82721722+rafaykhan-source@users.noreply.github.com>
2023-08-11 22:09:25 -0400

    Add event class and constructor

--- ADTs/event.py
+++ ADTs/event.py
@@ -9,3 +9,17 @@ event = Event(name="Community Gathering", size_limit=15)
 if not event.is_full():
     event.add_person(person)
 """
+
+
+class Event:
+    """A simple class that wraps an event's information and supports
+    event-related operations.
+    """
+
+    def __init__(self, name: str, size_limit: int) -> None:
+        self.name = name
+        "The event's name."
+        self.size_limit = size_limit
+        "The occupancy limit for the event."
+        self.__attendees = []
+        return
