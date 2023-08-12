06453ef97a8eb02521860813b650e8e4f2efe383
Rafay <82721722+rafaykhan-source@users.noreply.github.com>
2023-08-11 21:55:03 -0400

    Add module docstring to person.py

---
+++ ADTs/person.py
@@ -0,0 +1,13 @@
+"""This module contains the Person Abstract Datatype (ADT).
+
+Provided a Person's information from a spreadsheet, this class
+functions primarily as a wrapper of one's information, their
+ranked choices and supports operations.
+
+Typical usage example:
+
+person = Person(name="John Doe", 
+                id="jd0000", 
+                choices=["choice1", "choice2", "choice3"])
+"""
+
