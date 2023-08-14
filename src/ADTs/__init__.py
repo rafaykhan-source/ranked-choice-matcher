c491f5a6332688b9f95499491c32a4b5ee12230b
Rafay <82721722+rafaykhan-source@users.noreply.github.com>
2023-08-14 15:48:14 -0400

    Add package docstring to ADTs

--- src/ADTs/__init__.py
+++ src/ADTs/__init__.py
@@ -1,2 +1,7 @@
+"""This package is a collection of several Abstract Datatypes (ADTs).
+
+It contains the Person Module and Event Module, for representing 
+available events and people to be place on those event rosters.
+"""
 from ADTs.person import Person
 from ADTs.event import Event
