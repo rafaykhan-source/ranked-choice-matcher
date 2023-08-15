14487c44dfda3311c4e320a42a9619841e53a844
Rafay <82721722+rafaykhan-source@users.noreply.github.com>
2023-08-14 22:59:21 -0400

    Update dataproducer to read from csvs

--- ranked-choice-assigner/dataproducer.py
+++ ranked-choice-assigner/dataproducer.py
@@ -1,43 +1,80 @@
 """This module represents the data producer for the project."""
 
 from ADTs import Event, Person
+import pandas as pd
 
 
-def get_event_map() -> dict[str, Event]:
+def get_event_map(group_type: str) -> dict[str, Event]:
     """Returns a mapping of event name strings to event objects.
 
     Returns:
         dict[str, Event]: event map
     """
-    mapping = {
-        "Gathering 1": Event(name="Gathering 1", capacity=1),
-        "Gathering 2": Event(name="Gathering 2", capacity=0),
-        "Gathering 3": Event(name="Gathering 3", capacity=0),
-        "Gathering 4": Event(name="Gathering 4", capacity=3),
-    }
+    if group_type == "wellness":
+        data = __load_data("wellness_events.csv")
+    else:
+        data = __load_data("community_events.csv")
+    events = list(map(__create_event, data.values.tolist()))
+    
+    event_map = {}
+    for event in events:
+        event_map[event.name] = event
 
-    return mapping
+    return event_map
 
 
-def get_people() -> list[Person]:
-    """Returns people who must be listed on an event.
+def __load_data(csv_name: str) -> pd.DataFrame:
+    """Loads data from csv with specified name.
+
+    Args:
+        csv_name (str): Name of the csv for data retrieval.
 
     Returns:
-        list[Person]: people who must be listed on an event.
+        pd.DataFrame: data
     """
-    people = [
-        Person(
-            name="John Doe",
-            id="jd0000",
-            choices=["Gathering 1", "Gathering 2"],
-            top_choice="Gathering 1",
-        ),
-        Person(
-            name="Jane Doe",
-            id="jd0001",
-            choices=["Gathering 1", "Gathering 3"],
-            top_choice="Gathering 1",
-        ),
-    ]
+    return pd.read_csv(f"ranked-choice-assigner/processed-data/{csv_name}")
+
+
+def __create_event(row: list) -> Event:
+    return Event(
+        name=row[0].strip(),
+        capacity=row[1],
+    )
+
+
+def __create_person(row: list) -> Person:
+    return Person(
+        name=row[0].strip(),
+        id=row[1],
+        choices=row[2].split(","),
+        top_choice=row[3],
+    )
+
+
+def get_people(group_type: str) -> list[Person]:
+    """Returns a list of Person objects.
+
+    Returns:
+        list[Person]: people in group
+    """
+    if group_type == "wellness":
+        data = __load_data("wellness.csv")
+    else:
+        data = __load_data("community.csv")
+    people = list(map(__create_person, data.values.tolist()))
 
     return people
+
+
+def main() -> None:
+    """Unit Testing."""
+    people = get_people("wellness")
+
+    for person in people:
+        print(person)
+        print("------------")
+    return
+
+
+if __name__ == "__main__":
+    main()
