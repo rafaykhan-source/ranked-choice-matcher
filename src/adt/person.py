9582542d307a38dab5b43e8dc2ca3953e4298577
Rafay <82721722+rafaykhan-source@users.noreply.github.com>
2023-12-29 19:04:40 -0500

    Change person ADT to dataclass

--- src/adt/person.py
+++ src/adt/person.py
@@ -15,38 +15,23 @@ person = Person(
 
 """
 
+from dataclasses import dataclass
 
+
+@dataclass(slots=True)
 class Person:
-    """A class wrapping a person's information."""
+    """Wraps the information associated with a person."""
 
-    def __init__(
-        self,
-        name: str,
-        email: str,
-        choices: list[str],
-        top_choice: str,
-        placement: str = "",
-    ) -> None:
-        """Instantiates the person class.
-
-        Args:
-            name (str): Person's name.
-            email (str): Person's unique email.
-            choices (list[str]): Person's event choices.
-            top_choice (str): Person's top event choice.
-            placement (str, optional): Person's placement (if known). Defaults to "".
-        """
-        self.name: str = name
-        "The person's name. Instance Variable."
-        self.email: str = email
-        "The person's unique identifier/email. Instance Variable."
-        self.choices: list[str] = choices
-        "The person's event choices' names. Instance Variable."
-        self.top_choice: str = top_choice
-        "The person's top_choice event name. Instance Variable."
-        self.placement: str = placement
-        "The person's placed event name. Instance Variable."
-        return
+    name: str
+    "The person's name. Instance Variable."
+    email: str
+    "The person's unique identifier/email. Instance Variable."
+    choices: list[str]
+    "The person's event choices' names. Instance Variable."
+    top_choice: str
+    "The person's top_choice event name. Instance Variable."
+    placement: str = ""
+    "The person's placed event name. Optional. Instance Variable."
 
     def __str__(self) -> str:
         """Returns a string representation of the person.
@@ -54,8 +39,11 @@ class Person:
         Returns:
             str: Person's information.
         """
-        return f"""Name: {self.name}; ID: {self.email};
-Choices: {self.choices}; Top Choice: {self.top_choice}"""
+        return f"""Name: {self.name}
+ID: {self.email}
+Choices: {self.choices}
+Top Choice: {self.top_choice}
+Placement: {self.placement}"""
 
 
 def main() -> None:
