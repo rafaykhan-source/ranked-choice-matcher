49af94b473765f740f6ec2e521327bae779538ce
Rafay <82721722+rafaykhan-source@users.noreply.github.com>
2024-06-01 22:38:14 -0400

    Format __init__.py of adt with ruff

--- src/adt/__init__.py
+++ src/adt/__init__.py
@@ -3,5 +3,6 @@
 It contains the Person Module and Event Module, for representing
 available events and people to be place on those event rosters.
 """
+
 from adt.event import Event
 from adt.person import Person
